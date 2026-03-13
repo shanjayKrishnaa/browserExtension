50d8267c58cd1702213c7792d43df4d0bad5b2af
shanjayKrishnaa22 <shanjaykrish22@gmail.com>
2026-03-13 23:31:09 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 // function saveBtn(){
 //     console.log("Button clicked");
 // }
-let  myLeads=[];
-const inputEl=document.getElementById("input-el")
-const inputBtn=document.getElementById("input-btn")
-const ulEl=document.getElementById("ul-el")
+let myLeads = ["https://google.com"];
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 
 
-inputBtn.addEventListener("click",function(){
+inputBtn.addEventListener("click", function () {
     // console.log("button clciked through event listener")
     myLeads.push(inputEl.value)
-    inputEl.value=""
-    
+    inputEl.value = ""
+
     renderLeads()
 })
-function renderLeads(){
+function renderLeads() {
 
-   
-    let listItems=""
-    for(let i=0;i<myLeads.length;i++){
-        listItems+=`<li> ${myLeads[i]} </li>`
+
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li> 
+        <a target="_blank" href="#"> 
+        ${myLeads[i]} 
+        </a> 
+        </li>`
         // ulEl.innerHTML+=`<li> ${myLeads[i]} </li>`
-    
+
         // const li=document.createElement("li")  //create element
         // li.textContent=myLeads[i]              //set textContent    
         // ulEl.append(li)                        //append to unorderedList
-        
+
     }
-      ulEl.innerHTML=listItems
+    ulEl.innerHTML = listItems
 
 }
