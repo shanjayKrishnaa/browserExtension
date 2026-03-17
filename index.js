d29f353bf84e1a965d276d8b340dbfe231c40ffd
shanjayKrishnaa22 <shanjaykrish22@gmail.com>
2026-03-17 23:50:33 +0530

    data stored after refresh

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    renderLeads()
+}
 
 
 inputBtn.addEventListener("click", function () {
@@ -13,7 +18,9 @@ inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
 
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
+    // console.log(localStorage.getItem("myLeads"))
 })
 function renderLeads() {
 
@@ -21,7 +28,7 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `<li> 
-        <a target="_blank" href="#"> 
+        <a target="_blank" href='${myLeads[i]}'> 
         ${myLeads[i]} 
         </a> 
         </li>`
